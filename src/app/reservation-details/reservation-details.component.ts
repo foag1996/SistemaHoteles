ee17d8e45f1cc2c5e856997f5f386fbe6ae9fe11
foag1996 <foalvarezg@ufpso.edu.co>
2024-05-31 12:33:02 -0500

    Creacion de Componentes y Servicios

---
+++ src/app/reservation-details/reservation-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './reservation-details.component.html',
+  styleUrl: './reservation-details.component.css'
+})
+export class ReservationDetailsComponent {
+
+}
---
+++ src/app/reservation-management/reservation-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './reservation-management.component.html',
+  styleUrl: './reservation-management.component.css'
+})
+export class ReservationManagementComponent {
+
+}
